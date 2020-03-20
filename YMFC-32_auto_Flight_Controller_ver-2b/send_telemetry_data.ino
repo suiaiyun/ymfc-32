97b8536f32399e7da60644f257674757315c16dd
suilihui <466066151@qq.com>
2020-03-20 09:29:11 +0800

    send_telemetry_data增加延时

--- YMFC-32_auto_Flight_Controller_ver-2b/send_telemetry_data.ino
+++ YMFC-32_auto_Flight_Controller_ver-2b/send_telemetry_data.ino
@@ -95,6 +95,6 @@ void send_telemetry_data(void) {
     }
     //Send a stop bit
     GPIOB_BASE->BSRR = 0b1 << 0;                                                              //Set output PB0 to 1;
+    delayMicroseconds(104);
   }
-  delay(1);
 }
