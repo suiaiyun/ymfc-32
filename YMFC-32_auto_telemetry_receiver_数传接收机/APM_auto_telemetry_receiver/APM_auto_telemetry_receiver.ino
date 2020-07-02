b02d0d3dea4c23c86ca2a232668f8fe7af0b90e1
suilihui <466066151@qq.com>
2020-07-02 14:54:41 +0800

    apm数传

--- YMFC-32_auto_telemetry_receiver_数传接收机/APM_auto_telemetry_receiver/APM_auto_telemetry_receiver.ino
+++ YMFC-32_auto_telemetry_receiver_数传接收机/APM_auto_telemetry_receiver/APM_auto_telemetry_receiver.ino
@@ -28,7 +28,7 @@
  * In case we need a second serial port for debugging
  * Comment this line if no serial debugging is needed
  */
-#define SOFT_SERIAL_DEBUGGING   
+//#define SOFT_SERIAL_DEBUGGING
 #ifdef SOFT_SERIAL_DEBUGGING
   /* Library to use serial debugging with a second board */
   #include <SoftwareSerial.h>
@@ -82,9 +82,9 @@ uint8_t minutes, seconds, alarm_sound;
 //解锁状态
 uint8_t start;
 uint8_t receive_start_detect;
-//罗盘指向
-uint8_t actual_compass_heading;
 uint8_t telemetry_lost;
+//罗盘指向
+int16_t actual_compass_heading;
 int16_t button_store;
 //高度
 int16_t altitude_meters, max_altitude_meters, max_altitude_from_eeprom;
@@ -110,6 +110,7 @@ void setup() {
 #ifdef SOFT_SERIAL_DEBUGGING
   // [DEB] Soft serial port start
   mySerial.begin(57600);
+  mySerial.println("Run debugging...");
 #endif
 
   // 配置LCD屏幕宽度
@@ -155,6 +156,8 @@ void loop() {
     previousMillis = currentMillis;
 
   }
+
+  mavlink_loop();
 
   #ifndef SOFT_SERIAL_DEBUGGING
   if(key_press_timer > 0){
@@ -330,6 +333,8 @@ void loop() {
     lcd.print(" Lost telemetry");
     lcd.setCursor(0, 1);
     lcd.print("   connection!"); 
+    // 发送数据请求
+    Mav_Request_Data();
     delay(1000);    
   }
   
@@ -389,348 +394,9 @@ void loop() {
   }
 */
 
-        
-  /* MAVLink */
-  /* The default UART header for your MCU */ 
-  int sysid = 1;                   ///< ID 20 for this airplane. 1 PX, 255 ground station
-  int compid = 158;                ///< The component sending the message
-  int type = MAV_TYPE_QUADROTOR;   ///< This system is an airplane / fixed wing
- 
-  /**
-   * Define the system type, in this case an airplane -> on-board controller
-   * uint8_t system_type = MAV_TYPE_FIXED_WING;
-   */
-  uint8_t system_type = MAV_TYPE_GENERIC;
-  uint8_t autopilot_type = MAV_AUTOPILOT_INVALID;
- 
-  uint8_t system_mode = MAV_MODE_PREFLIGHT; ///< Booting up
-  uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
-  uint8_t system_state = MAV_STATE_STANDBY; ///< System ready for flight
-  // Initialize the required buffers
-  mavlink_message_t msg;
-  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
- 
-  /**
-   * 发送心跳包
-   * mavlink_msg_heartbeat_pack(sysid,compid, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
-   */
-  mavlink_msg_heartbeat_pack(1,0, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
- 
-  // Copy the message to the send buffer
-  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
- 
-  /**
-   * Send the message with the standard UART send function
-   * uart0_send might be named differently depending on
-   * the individual microcontroller / library in use.
-   */
-  unsigned long currentMillisMAVLink = millis();
-  if (currentMillisMAVLink - previousMillisMAVLink >= next_interval_MAVLink) {
-    /* Guardamos la última vez que se cambió el modo */
-    previousMillisMAVLink = currentMillisMAVLink;
-    
-    Serial.write(buf, len);
-
-    //Mav_Request_Data();
-    num_hbs_pasados++;
-    if(num_hbs_pasados>=num_hbs) {
-
-      #ifdef SOFT_SERIAL_DEBUGGING
-        mySerial.println("Streams requested!");
-      #endif
-      
-      /* Request streams from Pixhawk */
-      Mav_Request_Data();
-      num_hbs_pasados=0;
-    }
-
-  }
-
-  // Check reception buffer
-  comm_receive();
-}
-
-void Mav_Request_Data()
-{
-  mavlink_message_t msg;
-  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
-
-
-  // STREAMS that can be requested
-  /*
-   * Definitions are in common.h: enum MAV_DATA_STREAM
-   *   
-   * MAV_DATA_STREAM_ALL=0, // Enable all data streams
-   * MAV_DATA_STREAM_RAW_SENSORS=1, /* Enable IMU_RAW, GPS_RAW, GPS_STATUS packets.
-   * MAV_DATA_STREAM_EXTENDED_STATUS=2, /* Enable GPS_STATUS, CONTROL_STATUS, AUX_STATUS
-   * MAV_DATA_STREAM_RC_CHANNELS=3, /* Enable RC_CHANNELS_SCALED, RC_CHANNELS_RAW, SERVO_OUTPUT_RAW
-   * MAV_DATA_STREAM_RAW_CONTROLLER=4, /* Enable ATTITUDE_CONTROLLER_OUTPUT, POSITION_CONTROLLER_OUTPUT, NAV_CONTROLLER_OUTPUT.
-   * MAV_DATA_STREAM_POSITION=6, /* Enable LOCAL_POSITION, GLOBAL_POSITION/GLOBAL_POSITION_INT messages.
-   * MAV_DATA_STREAM_EXTRA1=10, /* Dependent on the autopilot
-   * MAV_DATA_STREAM_EXTRA2=11, /* Dependent on the autopilot
-   * MAV_DATA_STREAM_EXTRA3=12, /* Dependent on the autopilot
-   * MAV_DATA_STREAM_ENUM_END=13,
-   * 
-   * Data in PixHawk available in:
-   *  - Battery, amperage and voltage (SYS_STATUS) in MAV_DATA_STREAM_EXTENDED_STATUS
-   *  - Gyro info (IMU_SCALED) in MAV_DATA_STREAM_EXTRA1
-   */
-
-  // To be setup according to the needed information to be requested from the Pixhawk
-  const int  maxStreams = 3;
-  const uint8_t MAVStreams[maxStreams] = {MAV_DATA_STREAM_EXTENDED_STATUS, 
-                                          MAV_DATA_STREAM_EXTRA1, 
-                                          MAV_DATA_STREAM_EXTRA2};
-  const uint16_t MAVRates[maxStreams] = {0x01, 0x01, 0x01};
-
-    
-  for (int i=0; i < maxStreams; i++) {
-    /*
-     * mavlink_msg_request_data_stream_pack(system_id, component_id, 
-     *    &msg, 
-     *    target_system, target_component, 
-     *    MAV_DATA_STREAM_POSITION, 10000000, 1);
-     *    
-     * mavlink_msg_request_data_stream_pack(uint8_t system_id, uint8_t component_id, 
-     *    mavlink_message_t* msg,
-     *    uint8_t target_system, uint8_t target_component, uint8_t req_stream_id, 
-     *    uint16_t req_message_rate, uint8_t start_stop)
-     * 
-     */
-    mavlink_msg_request_data_stream_pack(2, 200, &msg, 1, 0, MAVStreams[i], MAVRates[i], 1);
-    uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-    Serial.write(buf, len);
-  }
-  
-  // Request: PARAM_REQUEST_LIST. Only for full log recording
-  /*
-   * Primitive: mavlink_msg_param_request_list_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-                   uint8_t target_system, uint8_t target_component)
-   */
-
-
-  // Configure
-  uint8_t system_id=2;
-  uint8_t component_id=200;
-  // mavlink_message_t* msg;
-  uint8_t target_system=1;
-  uint8_t target_component=0;
-  
-  mavlink_msg_param_request_list_pack(system_id, component_id, &msg,
-    target_system, target_component);
-  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-
-  // Send
-  Serial.write(buf, len);
-  
-/*
-  // Configure
-  uint8_t system_id=2;
-  uint8_t component_id=200;
-  // mavlink_message_t* msg;
-  uint8_t target_system=1;
-  uint8_t target_component=0;
-
-  // Pack
-  mavlink_msg_param_request_list_pack(system_id, component_id, &msg,
-    target_system, target_component);
-  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-
-  // Send
-#ifdef SOFT_SERIAL_DEBUGGING
-    pxSerial.write(buf,len);
-#else
-    Serial.write(buf, len);
-#endif
-*/
 }
 
 
-
-void comm_receive() {
- 
-  mavlink_message_t msg;
-  mavlink_status_t status;
- 
-  /**
-   * Echo for manual debugging
-   * Serial.println("---Start---");
-   */
-
-  while(Serial.available()>0) {
-    uint8_t c = Serial.read();
-
-    /* Try to get a new message */
-    if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) 
-    {
-      if(telemetry_lost > 0){
-        telemetry_lost = 0;
-        page = 0;
-      }
-      last_receive = millis();
-      
-      /* Handle message */
-      switch(msg.msgid) 
-      {
-        case MAVLINK_MSG_ID_HEARTBEAT:  // #0: Heartbeat
-          {
-            /**
-             * E.g. read GCS heartbeat and go into
-             * comm lost mode if timer times out
-             */
-            #ifdef SOFT_SERIAL_DEBUGGING
-              mySerial.println("PX HB");
-            #endif
-          }
-          break;
-
-        case MAVLINK_MSG_ID_SYS_STATUS:  // #1: SYS_STATUS
-          {
-            /* Message decoding: PRIMITIVE
-             * mavlink_msg_sys_status_decode(const mavlink_message_t* msg, mavlink_sys_status_t* sys_status)
-             */
-            //mavlink_message_t* msg;
-            mavlink_sys_status_t sys_status;
-            mavlink_msg_sys_status_decode(&msg, &sys_status);
-            battery_voltage = (float)sys_status.voltage_battery / 1000;
-            if (battery_voltage < BATTERY_COMPARE_VOLTAGE) error = 1;
-            
-            #ifdef SOFT_SERIAL_DEBUGGING
-              mySerial.print("PX SYS STATUS: ");
-              mySerial.print("[Bat (V): ");
-              mySerial.print(battery_voltage);
-              mySerial.print("], [Bat (A): ");
-              mySerial.print(sys_status.current_battery);
-              mySerial.print("], [Comms loss (%): ");
-              mySerial.print(sys_status.drop_rate_comm);
-              mySerial.println("]");
-            #endif
-            
-          }
-          break;
-
-        case MAVLINK_MSG_ID_PARAM_VALUE:  // #22: PARAM_VALUE
-          {
-            /* Message decoding: PRIMITIVE
-             * mavlink_msg_param_value_decode(const mavlink_message_t* msg, mavlink_param_value_t* param_value)
-             */
-            //mavlink_message_t* msg;
-            mavlink_param_value_t param_value;
-            mavlink_msg_param_value_decode(&msg, &param_value);
-            
-            #ifdef SOFT_SERIAL_DEBUGGING
-              mySerial.println("PX PARAM_VALUE");
-              mySerial.println(param_value.param_value);
-              mySerial.println(param_value.param_count);
-              mySerial.println(param_value.param_index);
-              mySerial.println(param_value.param_id);
-              mySerial.println(param_value.param_type);
-              mySerial.println("------ Fin -------");
-            #endif
-          }
-          break;
-
-        case MAVLINK_MSG_ID_RAW_IMU:  // #27: RAW_IMU
-          {
-            /* Message decoding: PRIMITIVE
-             *    static inline void mavlink_msg_raw_imu_decode(const mavlink_message_t* msg, mavlink_raw_imu_t* raw_imu)
-             */
-            mavlink_raw_imu_t raw_imu;
-            mavlink_msg_raw_imu_decode(&msg, &raw_imu);
-            
-          #ifdef SOFT_SERIAL_DEBUGGING
-            mySerial.println("PX RAW IMU");
-            mySerial.println(raw_imu.xacc);
-          #endif
-          }
-          break;
-
-        case MAVLINK_MSG_ID_ATTITUDE:  // #30
-          {
-            /* Message decoding: PRIMITIVE
-             *    mavlink_msg_attitude_decode(const mavlink_message_t* msg, mavlink_attitude_t* attitude)
-             */
-            mavlink_attitude_t attitude;
-            mavlink_msg_attitude_decode(&msg, &attitude);
-            roll_angle = (int8_t)(attitude.roll * 100);
-            pitch_angle = (int8_t)(attitude.pitch * 100);
-            
-          #ifdef SOFT_SERIAL_DEBUGGING
-            mySerial.println("PX ATTITUDE");
-            mySerial.println(attitude.roll);
-            mySerial.println(attitude.pitch);
-            mySerial.println(roll_angle);
-            mySerial.println(pitch_angle);
-          #endif
-          }
-          break;
-
-        case MAVLINK_MSG_ID_GPS_RAW_INT:
-        {
-          mavlink_gps_raw_int_t gps_raw;
-          mavlink_msg_gps_raw_int_decode(&msg, &gps_raw);
-          l_lat_gps = gps_raw.lat;
-          l_lon_gps = gps_raw.lon;
-          number_used_sats = gps_raw.satellites_visible;
-          fix_type = gps_raw.fix_type;
-        
-          #ifdef SOFT_SERIAL_DEBUGGING
-            mySerial.println("PX GPS RAW");
-            mySerial.print("lat: ");
-            mySerial.println(l_lat_gps);
-            mySerial.print("lon: ");
-            mySerial.println(l_lon_gps);
-            mySerial.print("number_used_sats: ");
-            mySerial.println(number_used_sats);
-            mySerial.print("fix_type: ");
-            mySerial.println(fix_type);
-          #endif
-        }
-        break;
-
-        case MAVLINK_MSG_ID_NAV_CONTROLLER_OUTPUT:
-        {
-          mavlink_nav_controller_output_t nav_controller;
-          mavlink_msg_nav_controller_output_decode(&msg, &nav_controller);
-          #ifdef SOFT_SERIAL_DEBUGGING
-            mySerial.println("PX NAV CONTROLLER");
-            mySerial.println(nav_controller.nav_roll);
-            mySerial.println(nav_controller.nav_pitch);
-            mySerial.println(nav_controller.target_bearing);
-          #endif 
-        }
-        break;
-
-      case MAVLINK_MSG_ID_VFR_HUD:
-        mavlink_vfr_hud_t vfr_hud;
-        mavlink_msg_vfr_hud_decode(&msg, &vfr_hud);
-        actual_compass_heading = vfr_hud.heading;
-        altitude_meters = vfr_hud.alt;
-        
-        #ifdef SOFT_SERIAL_DEBUGGING
-          mySerial.println("PX VFR HUD");
-          mySerial.print("heading: ");
-          mySerial.println(actual_compass_heading);
-          mySerial.print("alt: ");
-          mySerial.println(altitude_meters);
-        #endif 
-        
-      break;
-        
-       default:
-        #ifdef SOFT_SERIAL_DEBUGGING
-          mySerial.print("--- Otros: ");
-          mySerial.print("[ID: ");
-          mySerial.print(msg.msgid);
-          mySerial.print("], [seq: ");
-          mySerial.print(msg.seq);
-          mySerial.println("]");
-        #endif
-       break;
-      }
-    }
-  }
-}
 
 /**
  * 中断程序
---
+++ YMFC-32_auto_telemetry_receiver_数传接收机/APM_auto_telemetry_receiver/apm_mavlink.ino
@@ -0,0 +1,316 @@
+void mavlink_loop()
+{
+  /* MAVLink */
+  /* The default UART header for your MCU */ 
+  int sysid = 1;                   ///< ID 20 for this airplane. 1 PX, 255 ground station
+  int compid = 158;                ///< The component sending the message
+  int type = MAV_TYPE_QUADROTOR;   ///< This system is an airplane / fixed wing
+ 
+  /**
+   * Define the system type, in this case an airplane -> on-board controller
+   * uint8_t system_type = MAV_TYPE_FIXED_WING;
+   */
+  uint8_t system_type = MAV_TYPE_GENERIC;
+  uint8_t autopilot_type = MAV_AUTOPILOT_INVALID;
+ 
+  uint8_t system_mode = MAV_MODE_PREFLIGHT; ///< Booting up
+  uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
+  uint8_t system_state = MAV_STATE_STANDBY; ///< System ready for flight
+  // Initialize the required buffers
+  mavlink_message_t msg;
+  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+ 
+  /**
+   * 发送心跳包
+   * mavlink_msg_heartbeat_pack(sysid,compid, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
+   */
+  mavlink_msg_heartbeat_pack(1,0, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
+ 
+  // Copy the message to the send buffer
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+ 
+  /**
+   * Send the message with the standard UART send function
+   * uart0_send might be named differently depending on
+   * the individual microcontroller / library in use.
+   */
+  unsigned long currentMillisMAVLink = millis();
+  if (currentMillisMAVLink - previousMillisMAVLink >= next_interval_MAVLink) {
+    /* Guardamos la última vez que se cambió el modo */
+    previousMillisMAVLink = currentMillisMAVLink;
+    
+    Serial.write(buf, len);
+
+    //Mav_Request_Data();
+    num_hbs_pasados++;
+    if(num_hbs_pasados>=num_hbs) {
+
+      #ifdef SOFT_SERIAL_DEBUGGING
+        mySerial.println("Streams requested!");
+      #endif
+      
+      /* Request streams from Pixhawk */
+      Mav_Request_Data();
+      num_hbs_pasados=0;
+    }
+
+  }
+
+  // Check reception buffer
+  comm_receive();
+}
+
+void Mav_Request_Data()
+{
+  mavlink_message_t msg;
+  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+
+
+  // STREAMS that can be requested
+  /*
+   * Definitions are in common.h: enum MAV_DATA_STREAM
+   *   
+   * MAV_DATA_STREAM_ALL=0, // Enable all data streams
+   * MAV_DATA_STREAM_RAW_SENSORS=1, /* Enable IMU_RAW, GPS_RAW, GPS_STATUS packets.
+   * MAV_DATA_STREAM_EXTENDED_STATUS=2, /* Enable GPS_STATUS, CONTROL_STATUS, AUX_STATUS
+   * MAV_DATA_STREAM_RC_CHANNELS=3, /* Enable RC_CHANNELS_SCALED, RC_CHANNELS_RAW, SERVO_OUTPUT_RAW
+   * MAV_DATA_STREAM_RAW_CONTROLLER=4, /* Enable ATTITUDE_CONTROLLER_OUTPUT, POSITION_CONTROLLER_OUTPUT, NAV_CONTROLLER_OUTPUT.
+   * MAV_DATA_STREAM_POSITION=6, /* Enable LOCAL_POSITION, GLOBAL_POSITION/GLOBAL_POSITION_INT messages.
+   * MAV_DATA_STREAM_EXTRA1=10, /* Dependent on the autopilot
+   * MAV_DATA_STREAM_EXTRA2=11, /* Dependent on the autopilot
+   * MAV_DATA_STREAM_EXTRA3=12, /* Dependent on the autopilot
+   * MAV_DATA_STREAM_ENUM_END=13,
+   * 
+   * Data in PixHawk available in:
+   *  - Battery, amperage and voltage (SYS_STATUS) in MAV_DATA_STREAM_EXTENDED_STATUS
+   *  - Gyro info (IMU_SCALED) in MAV_DATA_STREAM_EXTRA1
+   */
+
+  // To be setup according to the needed information to be requested from the Pixhawk
+  const int  maxStreams = 3;
+  const uint8_t MAVStreams[maxStreams] = {MAV_DATA_STREAM_EXTENDED_STATUS, 
+                                          MAV_DATA_STREAM_EXTRA1, 
+                                          MAV_DATA_STREAM_EXTRA2};
+  const uint16_t MAVRates[maxStreams] = {0x02, 0x02, 0x02};
+
+    
+  for (int i=0; i < maxStreams; i++) {
+    /*
+     * mavlink_msg_request_data_stream_pack(system_id, component_id, 
+     *    &msg, 
+     *    target_system, target_component, 
+     *    MAV_DATA_STREAM_POSITION, 10000000, 1);
+     *    
+     * mavlink_msg_request_data_stream_pack(uint8_t system_id, uint8_t component_id, 
+     *    mavlink_message_t* msg,
+     *    uint8_t target_system, uint8_t target_component, uint8_t req_stream_id, 
+     *    uint16_t req_message_rate, uint8_t start_stop)
+     * 
+     */
+    mavlink_msg_request_data_stream_pack(2, 200, &msg, 1, 0, MAVStreams[i], MAVRates[i], 1);
+    uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+    Serial.write(buf, len);
+  }
+  
+  // Request: PARAM_REQUEST_LIST. Only for full log recording
+  /*
+   * Primitive: mavlink_msg_param_request_list_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                   uint8_t target_system, uint8_t target_component)
+   */
+
+
+/*
+  // Configure
+  uint8_t system_id=2;
+  uint8_t component_id=200;
+  // mavlink_message_t* msg;
+  uint8_t target_system=1;
+  uint8_t target_component=0;
+
+  // Pack
+  mavlink_msg_param_request_list_pack(system_id, component_id, &msg,
+    target_system, target_component);
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+
+  // Send
+#ifdef SOFT_SERIAL_DEBUGGING
+    pxSerial.write(buf,len);
+#else
+    Serial.write(buf, len);
+#endif
+*/
+}
+
+
+
+void comm_receive() {
+ 
+  mavlink_message_t msg;
+  mavlink_status_t status;
+ 
+  /**
+   * Echo for manual debugging
+   * Serial.println("---Start---");
+   */
+
+  while (Serial.available() > 0) 
+  {
+    uint8_t c = Serial.read();
+
+    /* Try to get a new message */
+    if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) 
+    {
+      if(telemetry_lost > 0)
+      {
+        telemetry_lost = 0;
+        page = 0;
+      }
+      last_receive = millis();
+      
+      /* Handle message */
+      switch(msg.msgid) 
+      {
+        case MAVLINK_MSG_ID_HEARTBEAT:  // #0: Heartbeat
+        {
+          /**
+           * E.g. read GCS heartbeat and go into
+           * comm lost mode if timer times out
+           */
+          #ifdef SOFT_SERIAL_DEBUGGING
+            mySerial.println("PX HB");
+          #endif
+        }
+        break;
+
+        case MAVLINK_MSG_ID_SYS_STATUS:  // #1: SYS_STATUS
+        {
+          /* Message decoding: PRIMITIVE
+           * mavlink_msg_sys_status_decode(const mavlink_message_t* msg, mavlink_sys_status_t* sys_status)
+           */
+          //mavlink_message_t* msg;
+          mavlink_sys_status_t sys_status;
+          mavlink_msg_sys_status_decode(&msg, &sys_status);
+          battery_voltage = (float)sys_status.voltage_battery / 1000;
+          if (battery_voltage < BATTERY_COMPARE_VOLTAGE) error = 1;
+          
+          #ifdef SOFT_SERIAL_DEBUGGING
+            mySerial.print("#");
+            mySerial.print(MAVLINK_MSG_ID_PARAM_VALUE);
+            mySerial.println(", battery_voltage: ");
+            mySerial.println(battery_voltage);
+          #endif
+        }
+        break;
+
+        case MAVLINK_MSG_ID_ATTITUDE:  // #30
+        {
+          /* Message decoding: PRIMITIVE
+           *    mavlink_msg_attitude_decode(const mavlink_message_t* msg, mavlink_attitude_t* attitude)
+           */
+          mavlink_attitude_t attitude;
+          mavlink_msg_attitude_decode(&msg, &attitude);
+          roll_angle = (int8_t)(attitude.roll * 100);
+          pitch_angle = (int8_t)(attitude.pitch * 100);
+          
+          #ifdef SOFT_SERIAL_DEBUGGING
+            mySerial.print("#");
+            mySerial.print(MAVLINK_MSG_ID_ATTITUDE);
+            mySerial.print(", roll: ");
+            mySerial.print(attitude.roll);
+            mySerial.print(", pitch: ");
+            mySerial.println(attitude.pitch);
+          #endif
+        }
+        break;
+
+        case MAVLINK_MSG_ID_GPS_RAW_INT:
+        {
+          mavlink_gps_raw_int_t gps_raw;
+          mavlink_msg_gps_raw_int_decode(&msg, &gps_raw);
+          l_lat_gps = gps_raw.lat;
+          l_lon_gps = gps_raw.lon;
+          number_used_sats = gps_raw.satellites_visible;
+          fix_type = gps_raw.fix_type;
+        
+          #ifdef SOFT_SERIAL_DEBUGGING
+            mySerial.print("#");
+            mySerial.print(MAVLINK_MSG_ID_GPS_RAW_INT);
+            mySerial.print(", lat: ");
+            mySerial.print(l_lat_gps);
+            mySerial.print(", lon: ");
+            mySerial.print(l_lon_gps);
+            mySerial.print(", alt: ");
+            mySerial.print(gps_raw.alt);
+            mySerial.print(", number_used_sats: ");
+            mySerial.print(number_used_sats);
+            mySerial.print(", fix_type: ");
+            mySerial.println(fix_type);
+          #endif
+        }
+        break;
+
+        case MAVLINK_MSG_ID_VFR_HUD:
+        {
+          mavlink_vfr_hud_t vfr_hud;
+          mavlink_msg_vfr_hud_decode(&msg, &vfr_hud);
+          altitude_meters = vfr_hud.alt;
+          actual_compass_heading = vfr_hud.heading;
+          
+          #ifdef SOFT_SERIAL_DEBUGGING
+            mySerial.print("#");
+            mySerial.print(MAVLINK_MSG_ID_VFR_HUD);
+            mySerial.print(", alt: ");
+            mySerial.print(altitude_meters);
+            mySerial.print(", airspeed: ");
+            mySerial.print(vfr_hud.airspeed);
+            mySerial.print(", groundspeed: ");
+            mySerial.print(vfr_hud.groundspeed);
+            mySerial.print(", climb: ");
+            mySerial.print(vfr_hud.climb);
+            mySerial.print(", throttle: ");
+            mySerial.println(vfr_hud.throttle);
+            
+          #endif
+        }
+        break;
+
+        case MAVLINK_MSG_ID_NAV_CONTROLLER_OUTPUT: // #62
+        {
+          mavlink_nav_controller_output_t nco;
+          mavlink_msg_nav_controller_output_decode(&msg, &nco);
+  
+          #ifdef SOFT_SERIAL_DEBUGGING
+            mySerial.print("#");
+            mySerial.print(MAVLINK_MSG_ID_NAV_CONTROLLER_OUTPUT);
+            mySerial.print(", nav_roll: ");
+            mySerial.print(nco.nav_roll);
+            mySerial.print(", nav_pitch: ");
+            mySerial.print(nco.nav_pitch);
+            mySerial.print(", nav_bearing: ");
+            mySerial.print(nco.nav_bearing);
+            mySerial.print(", target_bearing: ");
+            mySerial.print(nco.target_bearing);
+            
+            mySerial.print(", wp_dist: ");
+            mySerial.println(nco.wp_dist);
+          #endif
+        }
+        break;
+        
+       default:
+       {
+        #ifdef SOFT_SERIAL_DEBUGGING
+          mySerial.println("");
+          mySerial.print("--- Otros: ");
+          mySerial.print("ID: ");
+          mySerial.print(msg.msgid);
+          mySerial.print(", seq: ");
+          mySerial.print(msg.seq);
+          mySerial.println("");
+        #endif
+       }
+       break;
+      }
+    }
+  }
+}
